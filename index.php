05755c7508a5c73932d5c10514a3560a5add5cce
M. Akhyar Rahman H <akhyarrh@users.noreply.github.com>
2020-11-30 23:35:25 +0800

    Looks like we can't afford the iPhone after all (after taxes)!

--- index.php
+++ index.php
@@ -16,28 +16,25 @@
       $products['iPhone']=1000;
       $products['Toaster']=75;
 
+      $taxRate=0.0825;
+
       foreach($products as $key => $value){
         echo "<p>The ".$key." costs $".$value."</p>";
       }
-
-      echo "<h2>Items you can afford</h2>";
-
-      foreach($products as $key => $value){
-        if($value <= $credit ){
-          echo "<p>".$key." $".$value."</p>";
-        }
-      }
-      $amount=800;
-      $taxRate=0.0825;
-      $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-      echo "<p>Added tax: $".$addedTax."</p>";
 
       function tax_calc($amount,$tax){
         $calculate_tax = $amount*$tax;
         $amount = round($amount+$calculate_tax,2);
         return $amount;
       }
-      echo tax_calc(750,0.223);
+      echo "<h2>Items you can afford</h2>";
+
+      foreach($products as $key => $value){
+        $costWithTax = tax_calc($value,$taxRate);
+        if($costWithTax <= $credit ){
+          echo "<p>".$key." $".$value."</p>";
+        }
+      }
     ?>
   </body>
 </html>
